f564dfaf6e5243303524144058f62441f4106eb5
Vinicius Lima <106411366+ViniciusH97@users.noreply.github.com>
2024-02-26 22:51:34 -0300

    Add files via upload

---
+++ aula 8/padaria01.sql
@@ -0,0 +1,68 @@
+select idprato from prato where nome = 'pao de queijo'; -- descobrir o id Pela tabela prato
+select idcozinheiro from cozinheiro where id = 9; -- Após encontrar o id do prato 
+
+select * from cozinheiro;
+select * from prato;
+
+/*Quais cozinheiros fazem pao de queijo*/
+
+select cozinheiro.nome
+from cozinheiro 
+inner join prato on prato.idcozinheiro = cozinheiro.idcozinheiro
+where prato.nome = 'pao de queijo';
+
+select cozinheiro.nome, prato.nome
+from cozinheiro
+inner join prato on prato.idcozinheiro = cozinheiro.idcozinheiro;
+
+-- Quais os ingredientes são utilizados para fazer inhoque?
+
+select ingredientes.nome, prato.nome
+from receita
+inner join receita on receita.idprato = receita.idingredientes;
+
+select ingredientes.nome, prato.nome
+from ingredientes
+inner join receita on receita.idingredientes = receita.idprato
+where ingrediente.nome = 'inhoque';
+
+-- Correção
+
+select prato.nome, ingredientes.nome
+from prato
+inner join receita on receita.idprato = prato.idprato
+inner join ingredientes on ingredientes.idingredientes = receita.idingredientes
+where prato.nome = 'nhoque';
+
+-- Quais pratos utilizam ovo em suas receitas
+
+select prato.nome
+from prato
+inner join receita on receita.idprato = prato.idprato
+inner join ingredientes on ingredientes.idIngredientes = receita.idIngredientes
+where ingredientes.nome = 'ovo';
+
+-- Quais ingredientes utilizados pelo João e pelo Ricardo?
+
+select ingredientes.nome
+from cozinheiro
+inner join prato on prato.idcozinheiro = cozinheiro.idcozinheiro
+inner join receita on receita.idprato = prato.idprato
+inner join ingredientes on ingredientes.idIngredientes = receita.idIngredientes
+where cozinheiro.nome = 'joao' or cozinheiro.nome = 'ricardo';
+
+-- Sem repetições
+select distinct ingredientes.nome
+from cozinheiro
+inner join prato on prato.idcozinheiro = cozinheiro.idcozinheiro
+inner join receita on receita.idprato = prato.idprato
+inner join ingredientes on ingredientes.idIngredientes = receita.idIngredientes
+where cozinheiro.nome = 'joao' or cozinheiro.nome = 'ricardo';
+
+-- com nome do prato e do cozinheiro
+select ingredientes.nome, prato.nome, cozinheiro.nome
+from cozinheiro
+inner join prato on prato.idcozinheiro = cozinheiro.idcozinheiro
+inner join receita on receita.idprato = prato.idprato
+inner join ingredientes on ingredientes.idIngredientes = receita.idIngredientes
+where cozinheiro.nome = 'joao' or cozinheiro.nome = 'ricardo';
