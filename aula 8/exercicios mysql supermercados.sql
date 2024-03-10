58ae62d96fc648e73cd6c7c25fb0747d7a524c6d
Vinicius Lima <106411366+ViniciusH97@users.noreply.github.com>
2024-03-10 14:48:18 -0300

    Add files via upload

--- aula 8/exercicios mysql supermercados.sql
+++ aula 8/exercicios mysql supermercados.sql
@@ -1,7 +1,11 @@
 -- Exercícios
 
--- 1 - Quais os produtos do segmento de higiene?
-select * from produto
+select * from produto;
+select * from unidmedida;
+select * from segmento;
+
+-- 1 - Quantos produtos pertencem ao segmento de higiene?
+select count(*)produto from produto
 inner join segmento on segmento.idsegmproduto = produto.segmento
 where descsegmproduto = 'higiene';
 
@@ -13,12 +17,12 @@ where descsegmproduto = 'higiene';
 -- 3) Quantos produtos são vendidos em Litros? 
 select count(*) produto from produto
 inner join unidmedida on produto.unidademedida = unidmedida.idunidmedida
-where unidmedida.siglaunidmedida = 'lt';
+where unidmedida.descrunidmedida = 'Litros';
 
 -- 4) Quais os produtos que são vendidos em Litros?
 select distinct descproduto from produto
-inner join unidmedida on unidmedida.idunidmedida
-where unidmedida.siglaunidmedida = 'lt';
+inner join unidmedida on produto.unidademedida = unidmedida.idunidmedida
+where unidmedida.descrunidmedida = 'Litros';
 
 -- 5) Quantos produtos existem nos segmentos de Hortifruti e Carnes?
 select count(*) produto from produto
@@ -28,7 +32,7 @@ where descsegmproduto = 'hortifruti' or descsegmproduto = 'carnes';
 -- 6) Quantos produtos são vendidos em Litros?
 select count(*) produto from produto
 inner join unidmedida on produto.unidademedida = unidmedida.idunidmedida
-where unidmedida.siglaunidmedida = "lt";
+where unidmedida.descrunidmedida = 'Litros';
 
 -- 7) Quantos produtos são vendidos em Gramas e Kilos?
 select count(*) produto from produto
@@ -80,11 +84,10 @@ where segmento.descsegmproduto = "Higiene" or segmento.descsegmproduto = "Limpez
 or segmento.descsegmproduto = "Bebidas" and unidmedida.descrunidmedida = "Litros";
 
 -- 34) Qual o preço do produtos mais caro do segmento de Limpeza vendido em Litros?
-select valorunitario from produto
+select max(produto.valorunitario) from produto
 inner join segmento on produto.segmento = segmento.idsegmproduto
 inner join unidmedida on produto.unidademedida = unidmedida.idunidmedida
-where segmento.descsegmproduto = "Limpeza" and unidmedida.descrunidmedida = "Litros"
-order by valorunitario desc;
+where segmento.descsegmproduto = "Limpeza" and unidmedida.descrunidmedida = "Litros";
 
 -- 35) Qual o preço do produto mais barato do segmento de Higiene vendido em Rolos?
 select min(valorunitario) from produto
@@ -98,7 +101,7 @@ inner join unidmedida on produto.unidademedida = unidmedida.idunidmedida
 where unidmedida.descrunidmedida = "Litros" or unidmedida.descrunidmedida = "Mililitros";
 
 -- 37) Quantos são os produtos vendidos em Litros e Mililitros pertencentes ao segmento de Limpeza?
-select distinct descproduto from produto
+select count(*) descproduto from produto
 inner join unidmedida on produto.unidademedida = unidmedida.idunidmedida
 inner join segmento on produto.segmento - segmento.idsegmproduto
 where unidmedida.descrunidmedida = "Litros" or unidmedida.descrunidmedida = "Mililitros"
@@ -112,11 +115,15 @@ or unidademedida = 3 or unidademedida = 5 or unidmedida.descrunidmedida = "Milil
 unidademedida = 200 or unidademedida = 300 or unidademedida = 500;
 
 -- 39) Exiba o preço do produto mais caro e do mais barato pertencente do segmento de Limpeza?
-select max(valorunitario) from produto
-inner join segmento on produto.segmento = segmento.idsegmproduto
-where segmento.secrsegmproduto = "Limpeza";
+select
+	(select max(valorunitario) from produto
+	inner join segmento on produto.segmento = segmento.idsegmproduto
+	where segmento.descsegmproduto = "Limpeza") as preco_caro,
+	(select min(valorunitario) from produto
+	inner join segmento on produto.segmento = segmento.idsegmproduto
+	where segmento.descsegmproduto = "Limpeza") as preco_barato;
 
 -- 40) Quantos produtos existem no segmento de Hortifruti e Carnes?
 select count(*) produto from produto
 inner join segmento on produto.segmento = segmento.idsegmproduto
-where segmento.secrgmproduto = "Hortfruti" or segmento.secrgmproduto = "Carnes";
+where segmento.descsegmproduto = "Hortfruti" or segmento.descsegmproduto = "Carnes";
